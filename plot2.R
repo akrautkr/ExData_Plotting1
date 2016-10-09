dc4dd6c7f54ed87ed14b8fe836d2f711e0cb2c44
akrautkr <Alison.Krautkramer@gmail.com>
2016-10-08 19:49:32 -0500

    Code to create plots

---
+++ plot2.R
@@ -0,0 +1,125 @@
+## Program : plot2.R
+##
+## Course Project 1 : Exploratory Data Analysis from John Hopkins
+##
+## Written : A. Krautkramer, 10/8/2016
+##
+## Purpose : Obtain the electric power consumption dataset and plot line graph of 
+##           Date vs Global Active Power.  Create a PNG from the line graph
+##           named plot2.png.
+
+library(dplyr)
+
+## Function : downloadSource
+## 
+## Description : Download the electric power consumption data.  
+##               Returns a character vector containing the names of the unzipped files.
+downloadSource <- function (sourceURL, destfile) {
+        
+        ## Create Data Directory
+        if (!file.exists("./data")){dir.create("./data")}
+        
+        ## Download data into the Data Directory
+        download.file(sourceURL, destfile = destfile)
+        
+        ## Obtain the date the data was downloaded on and write to a file
+        dateDownloaded <- date()
+        
+        ## Write the date of the file download to a file
+        fileConn <- file("./data/downloadData.txt")
+        writeLines(dateDownloaded, fileConn)
+        close(fileConn)
+        
+        ## Unzip zip file
+        files <-unzip(zipfile = destfile, exdir="data")
+        
+        return(files)
+        
+}
+
+
+#### Function : importData
+##
+## Description : Read the downloaded data into a data frame
+##               Combine the date and time columns and change the class
+##               from character to POSIXlt.
+##               Add the column names
+importData <- function (filename) {
+        
+        na.string <- "?"
+        sep <- ";"
+        header <- TRUE
+        stopRead <- 94654
+        
+        powerData <- read.table (filename, na.string=na.string, sep=sep, 
+                                 header=header, nrows=stopRead)
+        
+        return(powerData)
+        
+}
+
+#### Fuction : formatData
+##
+## Description : Subsets the data for the specific days this assignment is interested in.
+##               Concatenate the date and time columns and convert the result from a
+##               character to a POSIXlt object.
+##
+formatData <- function(data) {
+        
+        #--- Subset data to include only the days for this assignment:
+        #--- 2007-02-01 and 2007-02-02
+        
+        data <- subset(data, (Date == "1/2/2007" | Date == "2/2/2007"))
+        
+        #--- Add a new column containing the concatentated date and time
+        data <- mutate(data, datetime = paste(Date, Time))
+        
+        #--- Convert the Date and Time columns to the POSIXlt class
+        data$datetime <- strptime(data$datetime, "%d/%m/%Y %H:%M:%S")
+        
+        return(data)
+        
+}
+
+#### Fuction : plotData
+##
+## Description : Plots the data and creates a png of a the results
+##
+plotData <- function(data) {
+        
+        png(filename="plot2.png")
+        with(data, plot(datetime, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
+        dev.off()
+}
+
+#######
+## START : Hard coded Data variables
+#######
+
+##--- source data
+sourceURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+##--local workstation destination
+destfile <- "./data/household_power_consumption.zip"
+
+##--- unziped filename
+unzipfile <- "./data/household_power_consumption.txt"
+
+
+#######
+## END : Hard coded Data variables
+#######
+
+### Main program function calls
+
+##-- download data and unzip the file into destfile
+files <- downloadSource(sourceURL, destfile)
+
+##-- read data from file into a data structure
+powerData <- importData(unzipfile)
+
+##-- reformat the data before plotting
+powerData <- formatData(powerData)
+
+##-- Plot Data
+plotData(powerData)
---
+++ plot4.R
@@ -0,0 +1,146 @@
+## Program : plot4.R
+##
+## Course Project 1 : Exploratory Data Analysis from John Hopkins
+##
+## Written : A. Krautkramer, 10/8/2016
+##
+## Purpose : Obtain the electric power consumption dataset and plot a 4 panel graph.  
+##           Create a PNG from the line graph
+##           named plot4.png.
+
+library(dplyr)
+
+## Function : downloadSource
+## 
+## Description : Download the electric power consumption data.  
+##               Returns a character vector containing the names of the unzipped files.
+downloadSource <- function (sourceURL, destfile) {
+        
+        ## Create Data Directory
+        if (!file.exists("./data")){dir.create("./data")}
+        
+        ## Download data into the Data Directory
+        download.file(sourceURL, destfile = destfile)
+        
+        ## Obtain the date the data was downloaded on and write to a file
+        dateDownloaded <- date()
+        
+        ## Write the date of the file download to a file
+        fileConn <- file("./data/downloadData.txt")
+        writeLines(dateDownloaded, fileConn)
+        close(fileConn)
+        
+        ## Unzip zip file
+        files <-unzip(zipfile = destfile, exdir="data")
+        
+        return(files)
+        
+}
+
+
+#### Function : importData
+##
+## Description : Read the downloaded data into a data frame
+##               Combine the date and time columns and change the class
+##               from character to POSIXlt.
+##               Add the column names
+importData <- function (filename) {
+        
+        na.string <- "?"
+        sep <- ";"
+        header <- TRUE
+        stopRead <- 94654
+        
+        powerData <- read.table (filename, na.string=na.string, sep=sep, 
+                                 header=header, nrows=stopRead)
+        
+        return(powerData)
+        
+}
+
+#### Fuction : formatData
+##
+## Description : Subsets the data for the specific days this assignment is interested in.
+##               Concatenate the date and time columns and convert the result from a
+##               character to a POSIXlt object.
+##
+formatData <- function(data) {
+        
+        #--- Subset data to include only the days for this assignment:
+        #--- 2007-02-01 and 2007-02-02
+        
+        data <- subset(data, (Date == "1/2/2007" | Date == "2/2/2007"))
+        
+        #--- Add a new column containing the concatentated date and time
+        data <- mutate(data, datetime = paste(Date, Time))
+        
+        #--- Convert the Date and Time columns to the POSIXlt class
+        data$datetime <- strptime(data$datetime, "%d/%m/%Y %H:%M:%S")
+        
+        return(data)
+        
+}
+
+#### Fuction : plotData
+##
+## Description : Plots the data and creates a png of a the results
+##
+plotData <- function(data) {
+        
+        png(filename="plot4.png", width=480, height=480, units="px")
+        
+        #-- Set up the panel as a 2x2 
+        par(mfrow=c(2,2))
+        
+        #--- Plot 1st plot
+        with(data, plot(datetime, Global_active_power, type="l", xlab="", ylab="Global Active Power"))
+        
+        #--- Plot 2nd plot
+        with(data, plot(datetime, Voltage, type="l", xlab="datetime", ylab="Voltage"))
+        
+        #--- Plot 3rd plot
+        with(data, plot(datetime, Sub_metering_1, type="n", xlab="", ylab="Energy sub metering"))
+        with(data, lines(datetime, Sub_metering_3, col="blue"))
+        with(data, lines(datetime, Sub_metering_2, col="red"))
+        with(data, lines(datetime, Sub_metering_1, col="black"))
+        
+        #--- Add legend to the 3rd plot
+        legend("topright", lty="solid", lwd=1, col=c("black","red", "blue"), 
+               legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty="n")
+        
+        #--- Plot 4th plot
+        with(data, plot(datetime, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power"))
+        dev.off()
+}
+
+#######
+## START : Hard coded Data variables
+#######
+
+##--- source data
+sourceURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+##--local workstation destination
+destfile <- "./data/household_power_consumption.zip"
+
+##--- unziped filename
+unzipfile <- "./data/household_power_consumption.txt"
+
+
+#######
+## END : Hard coded Data variables
+#######
+
+### Main program function calls
+
+##-- download data and unzip the file into destfile
+files <- downloadSource(sourceURL, destfile)
+
+##-- read data from file into a data structure
+powerData <- importData(unzipfile)
+
+##-- reformat the data before plotting
+powerData <- formatData(powerData)
+
+##-- Plot Data
+plotData(powerData)
